cc2d8dde9f691d1bf2c6559304c328c041bcc205
stundennull <stundennull@gmail.com>
2020-11-14 19:26:45 -0600

    initialzed and created @@all method for user/event/reservation

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
+    has_many :events, :reservations
+    has-many :events, through: :reservations
+
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
